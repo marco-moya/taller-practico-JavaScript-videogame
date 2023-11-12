2e3db600884f6cf0402ae86d80525cac2236bda2
Marco Moya <zgmf_x235@hotmail.com>
2023-11-12 13:39:42 -0600

    Agregando detección de colisiones fijas y con arrays

--- game.js
+++ game.js
@@ -1,5 +1,5 @@
-const d = document;
-const $canvas = d.querySelector('#game'),
+const d = document,
+  $canvas = d.querySelector('#game'),
   $game = $canvas.getContext('2d'),
   $btnUp = d.querySelector('#up'),
   $btnLeft = d.querySelector('#left'),
@@ -8,25 +8,33 @@ const $canvas = d.querySelector('#game'),
   playerPosition = {
     x: undefined,
     y: undefined,
+  },
+  giftPosition = {
+    x: undefined,
+    y: undefined,
   };
   
-let canvasSize,
-  elementsSize;
+  let canvasSize,
+  elementsSize,
+  enemyPosition = [];
 
 window.addEventListener('load', setCanvasSize);
 window.addEventListener('resize', setCanvasSize);
 
 function setCanvasSize() {
   if (window.innerHeight > window.innerWidth) {
-    canvasSize = Math.floor(window.innerWidth * 0.8);
+    canvasSize = window.innerWidth * 0.8;
+    canvasSize = Math.trunc(canvasSize);
   } else {
-    canvasSize = Math.floor(window.innerHeight * 0.8);
+    canvasSize = window.innerHeight * 0.8;
+    canvasSize = Math.trunc(canvasSize);
   }
 
   $canvas.setAttribute('width', canvasSize);
   $canvas.setAttribute('height', canvasSize);
 
   elementsSize = (canvasSize / 10);
+  elementsSize = Math.round(parseFloat(elementsSize.toFixed(2)));
 
   startGame();
 }
@@ -35,12 +43,11 @@ function startGame() {
   $game.font = elementsSize + 'px Verdana';
   $game.textAlign = 'end'
   const map = maps[0];
-  // Usando trim para eliminar los espacios vacíos laterales del elemento map y el metodo split para convertir el string en un nuevo array a partir de los saltos de linea "\n".
-  const mapRows = map.trim().split("\n");
-  // Usando el metodo array.map comvertimos cada elemento del mapRow en un array cuyo elementos son serados por el metodo split.
-  const mapRowsCols = mapRows.map(row => row.trim().split(""));
+  const mapRows = map.trim().split("\n"); // Usando trim para eliminar los espacios vacíos laterales del elemento map y el metodo split para convertir el string en un nuevo array a partir de los saltos de linea "\n".
+  const mapRowsCols = mapRows.map(row => row.trim().split("")); // Usando el metodo array.map comvertimos cada elemento del mapRow en un array cuyo elementos son creados por el metodo split.
   
-  $game.clearRect(0, 0, canvasSize, canvasSize);
+  enemyPosition = []; //limpia el array para no duplicar las colisiones cada vez que el jugador se mueve. 
+  $game.clearRect(0, 0, canvasSize, canvasSize); //limpia el mapa cada vez que el jugador se mueve.
   mapRowsCols.forEach((row, indexRow) => {
     row.forEach((col, indexCol) => {
       const emoji = emojis[col];
@@ -48,11 +55,17 @@ function startGame() {
       const posY = elementsSize * (indexRow + 1);
 
       if (col === 'O') {
-        if (playerPosition.x === undefined && playerPosition.y === undefined) {
+        if (!playerPosition.x && !playerPosition.y) {
           playerPosition.x = posX;
           playerPosition.y = posY;
         }
-      }
+      } else if (col === 'I') {
+        giftPosition.x = posX;
+        giftPosition.y = posY;
+      } else if (col === 'X') {
+        enemyPosition.push({x: posX, y: posY})
+      } 
+
 
       $game.fillText(emoji, posX, posY);
     });
@@ -61,11 +74,19 @@ function startGame() {
 }
 
 function movePlayer() {
+  const gifCollitionX = playerPosition.x == giftPosition.x;
+  const gifCollitionY = playerPosition.y == giftPosition.y;
+  const gifCollition = gifCollitionX && gifCollitionY
+  if (gifCollition) console.log('¡Sube de nivel!');
   $game.fillText(emojis.PLAYER, playerPosition.x, playerPosition.y);
-  console.log(elementsSize);
-  console.log(canvasSize);
-  console.log('x', playerPosition.x);
-  console.log('y', playerPosition.y);
+  console.log(enemyPosition);
+  
+  const enemyColition = enemyPosition.find(enemy => {
+    const enemyColitionX = enemy.x == playerPosition.x;
+    const enemyColitionY = enemy.y == playerPosition.y;
+    return enemyColitionX && enemyColitionY;
+  })
+  if (enemyColition) console.log('Chocaste');
 }
 
 d.addEventListener('click', (e) => {
